79448bd4f914d5f4053fec108fa41113cdb96681
Ashutosh Chaurasiya <ac.ch503@gmail.com>
2019-09-21 12:30:03 +0545

    added knexfile.js and index.js

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+	client: 'mysql',
+	connection: {
+		host: '127.0.0.1',
+		user: 'root',
+		password: 'root',
+		database: 'FinalYear'
+	},
+	
+}
