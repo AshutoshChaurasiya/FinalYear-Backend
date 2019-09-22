fab53ea88946be5718b96a7a6f3d92248a3da43d
Ashutosh Chaurasiya <ac.ch503@gmail.com>
2019-09-22 12:10:36 +0545

    adde file and make some changes in knexfile

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const Express = require('express');
+var bodyParser = require("body-parser");
+var cors = require("cors");
+// var path = require('path');
+
+// var uploadRouter = require('./routes/upload');
+
+//create express instance/object
+const express = new Express();
+express.use(cors());
+express.use(bodyParser.json());
+express.options('*', cors());
